25c763194d8aaec731961575421adeddbf2603e4
a.sushchikh <a.sushchikh@gmail.com>
2017-03-16 06:49:00 +0300

    +imgs, js-anime, form, more form, interactions

--- js/my.js
+++ js/my.js
@@ -40,6 +40,7 @@ $(window).scroll(function() {
 				$('.compl_element').eq(index).addClass('show');
 			}, 100 * (index + 1))
 		});
+		$('.tomato_container').addClass('fly');
 	};	
 
 // char element show
@@ -59,10 +60,144 @@ $(window).scroll(function() {
 
 
 
+
+// ########   #######   ######  ########
+// ##     ## ##     ## ##    ##    ##
+// ##     ## ##     ## ##          ##
+// ########  ##     ##  ######     ##
+// ##        ##     ##       ##    ##
+// ##        ##     ## ##    ##    ##
+// ##         #######   ######     ##
+
+  $("#form").submit(function() {
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('.form_hidden_wrapper').removeClass('show_form');
+      succsess();
+
+    });
+    return false;
+
+  });
+
+
+  $("#form_top").submit(function() {
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('.form_hidden_wrapper').removeClass('show_form');
+      succsess();
+    });
+    return false;
+
+  });
+
+
+  $("#form_hidden").submit(function() {
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('.form_hidden_wrapper').removeClass('show_form');
+      succsess();
+    });
+    return false;
+
+  });
+
+
+function succsess(){
+	$('.succsess_wrapper').addClass('succses_show');
+	setTimeout(function(){
+		$('.succsess_wrapper').removeClass('succses_show');
+	},5000);
+};
+
+
 // летающая помидорка
 
-$('.tomato_container').addClass('fly');
+
+
+
+
+
+$('.show_btn').click(function(event) {
+	$('.form_hidden_wrapper').addClass('show_form');
+});
+$('.close').click(function(event) {
+	$('.form_hidden_wrapper').removeClass('show_form');
+	$('.succsess_wrapper').removeClass('succses_show');
+});
 
 
 
 }); //$(document).ready-func END
+
+
+
+
+/*
+ ######   #######  ##     ## ##    ## ######## ########   #######  ##      ## ##    ##
+##    ## ##     ## ##     ## ###   ##    ##    ##     ## ##     ## ##  ##  ## ###   ##
+##       ##     ## ##     ## ####  ##    ##    ##     ## ##     ## ##  ##  ## ####  ##
+##       ##     ## ##     ## ## ## ##    ##    ##     ## ##     ## ##  ##  ## ## ## ##
+##       ##     ## ##     ## ##  ####    ##    ##     ## ##     ## ##  ##  ## ##  ####
+##    ## ##     ## ##     ## ##   ###    ##    ##     ## ##     ## ##  ##  ## ##   ###
+ ######   #######   #######  ##    ##    ##    ########   #######   ###  ###  ##    ##
+ */
+
+
+
+
+
+function CountdownTimer(elm,tl,mes){
+ this.initialize.apply(this,arguments);
+}
+CountdownTimer.prototype={
+ initialize:function(elm,tl,mes) {
+  this.elem = document.getElementById(elm);
+  this.tl = tl;
+  this.mes = mes;
+ },countDown:function(){
+  var timer='';
+  var today=new Date();
+  var day=Math.floor((this.tl-today)/(24*60*60*1000));
+  var hour=Math.floor(((this.tl-today)%(24*60*60*1000))/(60*60*1000));
+  var min=Math.floor(((this.tl-today)%(24*60*60*1000))/(60*1000))%60;
+  var sec=Math.floor(((this.tl-today)%(24*60*60*1000))/1000)%60%60;
+  var me=this;
+
+  if( ( this.tl - today ) > 0 ){
+   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">ДНИ</div><span class="number day">'+day+'</span></span>';
+   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">ЧАСЫ</div><span class="number hour">'+hour+'</span></span>';
+   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">МИН</div><span class="number min">'+this.addZero(min)+'</span></span><span class="number-wrapper"><div class="line"></div><div class="caption">СЕК</div><span class="number sec">'+this.addZero(sec)+'</span></span>';
+   this.elem.innerHTML = timer;
+   tid = setTimeout( function(){me.countDown();},10 );
+  }else{
+   this.elem.innerHTML = this.mes;
+   return;
+  }
+ },addZero:function(num){ return ('0'+num).slice(-2); }
+}
+function CDT(){
+
+ // Set countdown limit
+ var tl = new Date('2019/05/09 00:00:00');
+
+ // You can add time's up message here
+  var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div></span>');
+  timer.countDown();
+}
+window.onload=function(){
+ CDT();
+}
+
